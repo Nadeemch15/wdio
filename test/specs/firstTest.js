bbc795316a16bea97f17db89f565fe0e65b3292e
Nadeem <nadeemch15@yahoo.com>
2025-05-06 18:32:16 +0100

    first commit

--- test/specs/firstTest.js
+++ test/specs/firstTest.js
@@ -1,9 +1,30 @@
 describe('Ecommerce Application', async()=>
 {
-    it('Login Page', async ()=>
-    {
-       
+    xit('Login Page', async ()=>
+    {     
        await browser.url("https://rahulshettyacademy.com/locatorspractice/");
-       console.log(await browser.getTitle());
+       console.log(await browser.getTitle());       
+       await expect(browser).toHaveTitle(expect.stringContaining("Rahul Shetty"));
+       await $("//input[@id='inputUsername']").setValue("rahulshettyacademy");
+       await $("//input[@name='inputPassword']").setValue("learning");
+       await $("//button[@type='submit']").click();    
     })
+
+    it('Failed Login Page', async ()=>
+        {           
+           await browser.url("https://rahulshettyacademy.com/loginpagePractise/");
+           console.log(await browser.getTitle());       
+           await expect(browser).toHaveTitle(expect.stringContaining("Rahul Shetty"));
+           await $("//input[@id='username']").setValue("rahulshettyacademy");
+           await $("//input[@name='password']").setValue("learning123");
+           await $("#signInBtn").click();
+           console.log(await $(".alert-danger").getText());
+           await browser.waitUntil(async()=> $("#signInBtn").getAttribute('value') === 'Sign In',
+           {
+            timeout: 5000,
+            timeoutMsg: 'Error message is not showing up'
+        })
+        //await expect($("p").toHaveText(expect.stringContaining('user name is')));
+        //await expect($("p").toHaveText(expect.stringContaining('user name is rahulshettry'))
+    })  
 })
